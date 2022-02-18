acab3176e9ba62555ca46efc956297af08f9811e
Saurabha-Daa <99875345+Saurabha-Daa@users.noreply.github.com>
2022-02-18 13:39:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 13 02:20:31 2020
 
-@author: Krish Naik
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 12:50:04 2020
-
-@author: krish.naik
-"""
 
 
 import pandas as pd
@@ -22,10 +10,10 @@ import streamlit as st
 
 def main():
     st.write('Hello World')
-#    uploaded_file = st.file_uploader("Choose a file")
-#    if uploaded_file is not None:
-#        dataframe = pd.read_csv(uploaded_file)
-#        st.write(dataframe)
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
 
 if __name__=='__main__':
     main()
